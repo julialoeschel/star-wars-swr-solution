0d255a1a69a23d88e34c8abbbdb62b1b24202497
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-09 14:58:47 +0100

    Update fetcher

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function App({ Component, pageProps }) {
   return (
